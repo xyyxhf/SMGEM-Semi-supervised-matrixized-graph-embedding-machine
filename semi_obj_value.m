86c161a73b5fb541ce19f1ced4e99581412de512
Haifeng Xu <84455195+xyyxhf@users.noreply.github.com>
2025-03-18 21:51:53 +0800

    Delete semi_obj_value.m

--- semi_obj_value.m
+++
@@ -1,5 +0,0 @@
-function obj = semi_obj_value(w, b, Xl, XminusX, y,  nuc_sumvalue, tau, lambda)
-[~,~,l] = size(Xl);
-obj = 0.5 * (w') * w + (1/l) * sum(max(0, 1 - y .* (Xl * w + b))) + tau * nuc_sumvalue ...
-    + 0.5 * lambda * (sum(XminusX * w))^2;
-end
